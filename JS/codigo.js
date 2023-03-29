6af585c6d117fc3f8e12bc23638f234d40f6261f
SOFIA MAILÉN GRASSO BONALDO <47059737@ort>
2023-03-29 09:06:44 -0300

    varios cambios

--- JS/codigo.js
+++ JS/codigo.js
@@ -1,40 +1,63 @@
 //Lista con objetos
 let lista = [];
 
-let agregarElemento = () => {
+const agregarElemento = () => {
     const tiempoTranscurrido = Date.now();
     const hoy = new Date(tiempoTranscurrido);
     //hoy.toUTCString(); // Devuelve "Sat, 13 Jun 2020 18:30:00 GMT"
+    lista.push(objeto);
+    console.log(lista);
+    //Get element y create element
+    var ul = document.getElementById("list"); 
+    //var ElementoPrevio = document.getElementById("input"); 
+    var valor = document.getElementById("input").value;
+    
+    var li = document.createElement("li");
+    li.classList.add('list-group-item');
+    
+    var input = document.createElement("input");
+    input.type = 'checkbox';
+    input.classList.add('form-check-input', 'me-1');
+    input.value=0;
+    input.setAttribute("id", "forthCheckboxStretched");
+    
+    var label = document.createElement("label");
+    label.classList.add('form-check-label', 'stretched-link');
+    label.innerText = valor;
+    label.setAttribute("for", "forthCheckboxStretched");
+    
+    li.appendChild(input);
+    li.appendChild(label);
+    
+    //li.appendChild(document.createTextNode(valor));  //lo que está dentro del elemento ul
+    li.setAttribute("id", "element"); // setea el id
+    ul.appendChild(li);
+    
+    
+    //nuevoElemento.before(document.getElementById("element"));
+    //li.body.before(document.getElementById("element"));
     let objeto = {
+        elemento: li,
         fechacreacion: hoy,
         fechaTachado: null
     };
-    lista.push(objeto);
-    
-    //Get element y create element
-    var ElementoPrevio = document.getElementById("input"), textoElemento = document.getElementById("input").innerText;
-    var nuevoElemento = document.createElement("li");
-    nuevoElemento.innerText = textoElemento;
-
-    // añade el elemento creado y su contenido al DOM
-    //document.body.insertBefore(nuevoElemento, ElementoPrevio);
-
-    nuevoElemento.before(document.getElementById("element"));
-    //document.body.before(document.getElementById("element"));
 }
-opcionMasRapida = () => {
+
+opcionMasRapida = () => { //restar fechas, fechacreación - fechatachado
     var mayorTachado, elementosTachados;
     for(var i = 1; i <= lista.length; i++){
-        //if(){
-
-        //}
+        if(lista[i].fechaTachado!=null){
+            const fechaTachadoMs = lista[i].fechaTachado.
+            var resta = 
+        }
     }
 }
 
 const cambiarEstilo = () => {
     let element = document.getElementById("flexCheckDefault");
-    let element2 = document.getElementById("labelChackbox");
+    let element2 = document.getElementById("labelCheckbox");
     console.log(element.tachado);
+    
     if(element.tachado == true){
         element2.classList.add('chequeado')
     }
